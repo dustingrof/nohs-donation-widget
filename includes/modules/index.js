a9b769af740e301c57a0f405c7c636a14b154386
Dustin Grof <dustingrof@gmail.com>
2023-02-14 11:34:53 -0800

    Remove imports for Simple Header, Custom CTA

--- includes/modules/index.js
+++ includes/modules/index.js
@@ -1,5 +1,3 @@
 import DonationWidget from './DonationWidget/DonationWidget';
-import SimpleHeader from './SimpleHeader/SimpleHeader';
-import CustomCtaAllOptions from './CustomCTA/CustomCTA';
 
-export default [DonationWidget, SimpleHeader];
+export default [DonationWidge];
